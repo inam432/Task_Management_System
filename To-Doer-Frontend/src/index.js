104ec7abbb0515368ea854fcc987fd3cbe7638d1
inam432 <inam85813@gmail.com>
2026-08-09 16:45:52 +0500

    Project updated

--- To-Doer-Frontend/src/index.js
+++ To-Doer-Frontend/src/index.js
@@ -3,7 +3,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App, { Login } from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
